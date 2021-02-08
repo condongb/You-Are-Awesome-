ca9d89259df75beb0142499524960e42f1e46eff
gage <gage.condon@gmail.com>
2021-02-08 03:09:12 -0500

    Done with all week 1 challenges

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -15,17 +15,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("viewDidLoad has run!")
-        messageLabel.text = "Fabulous? That's You!"
     }
 
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
         print("The message button was pressed!")
         messageLabel.text = "You are awesome!"
+        messageLabel.textColor = UIColor.blue
+        messageLabel.textAlignment = .left
     }
     @IBAction func anotherMessageButtonPressed(_ sender: UIButton) {
         print("The another message button was pressed!")
         messageLabel.text = "You Are Great!"
+        messageLabel.textColor = UIColor.red
+        messageLabel.textAlignment = .right
     }
+    
 }
 
